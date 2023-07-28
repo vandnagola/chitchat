7d2fdd75f799b6b987240e845dd4e6993cdac0ac
= <vandnagola756@gmail.com>
2023-07-28 13:57:12 +0530

    add seesion and chatroom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'chatrooms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new'
 end
