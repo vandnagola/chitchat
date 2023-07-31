e9302640befc99f4ed8f384f891756265d4e5de8
= <vandnagola756@gmail.com>
2023-07-31 14:53:09 +0530

    add real time chat functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,8 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
+      ActionCable.server.broadcast "chatroom_channel",
+                                    mod_message: message_render(message)
       redirect_to root_path
     end
   end
@@ -12,6 +14,10 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
   end
 
 end
