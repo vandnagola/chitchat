d177f541a62d8a194ee47a9c63250c3b6810b6ea
= <vandnagola756@gmail.com>
2023-07-31 13:18:52 +0530

    add session code andview  flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'chatrooms#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
